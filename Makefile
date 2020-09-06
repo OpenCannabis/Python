67e093b18e790104e4f2406205267b3ac2d5de48
Sam Gammon <sam.g@cookies.co>
2020-09-06 15:45:10 -0700

    Version bump -> 0.0.1-alpha5

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@
 #
 
 PROJECT ?= ocpy
-VERSION ?= 0.0.1-alpha4
+VERSION ?= 0.0.1-alpha5
 PWD ?= $(shell pwd)
 ENV ?= $(PWD)/.env
 DIST ?= $(PWD)/dist
--- opencannabis/setup.py
+++ opencannabis/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='opencannabis',
-  version='0.0.1-alpha4',
+  version='0.0.1-alpha5',
   description='OpenCannabis SDK for Python',
   author='Sam Gammon',
   author_email='sam.g@cookies.co',
